93caf3350c2e3620520387e5f6e55d9056e19a5b
doublejeong <chungjungsoo@gmail.com>
2023-07-20 18:19:07 +0900

    230720

--- js/prac01.js
+++ js/prac01.js
@@ -1,37 +1,24 @@
-// window.onload = function(){
-//     var ce = document.getElementById("inputText");
-
-//     // ce.onclick = function(){
-//     //     ce.style.backgroundColor = "blue";
-//     // }
-
-//     var changeBlue = function(event){
-//         console.log(event);
-//         this.style.backgroundColor = "blue";
-
-//         for(var key in event){
-//             console.log(key + " : " + event[key]);
-//         }
-//     };
-//     ce.addEventListener("click",changeBlue);
-
-//     // ce.ondblclick = function(){
-//     //     ce.style.backgroundColor = "red";
-//     // }
-
-//     var changeRed = function(event){
-//         console.log(event);
-//         this.style.backgroundColor = "red";
-//     }
-//     ce.addEventListener("dblclick",changeRed);
-// }
-
-
 window.onload = function(){
-    var it = document.getElementById("inputText");
-    var op = document.getElementById("output");
 
-    it.onkeydown = function(e){
-        op.innerHTML = e.keyCode;
+    function inputHandler (event){
+        console.log(event.key);     //A
+        console.log(event.keyCode); //
+
+        if(event.key != 'Enter' && event.key != 'Shift'){
+            document.getElementById('output').innerText = event.key.charCodeAt();
+        }
     }
+
+    var input = document.getElementById("inputText");
+    input.addEventListener("keydown", inputHandler);
 }
+
+
+// window.onload = function(){
+//     var it = document.getElementById("inputText");
+//     var op = document.getElementById("output");
+
+//     it.onkeydown = function(e){
+//         op.innerHTML = e.keyCode;
+//     }
+// }
